ccbe5c4fd01f38d08b4a5bf69a9117e43eccb350
Rashi bajpai <168172417+rashibajpai12@users.noreply.github.com>
2026-06-19 01:21:58 +0530

    Update verification_agent.py

--- agents/verification_agent.py
+++ agents/verification_agent.py
@@ -1,6 +1,21 @@
-def verify_evidence(insight):
+import google.generativeai as genai
 
-    return {
-        "score": 9.4,
-        "status": "Verified"
-    }
+def verify_evidence(insight, result):
+
+    prompt = f"""
+Check if the insight is supported by the SQL result.
+
+Insight:
+{insight}
+
+SQL Result:
+{result}
+
+Give:
+1. Evidence Score out of 10
+2. Unsupported claims if any
+"""
+
+    response = genai.GenerativeModel("gemini-2.5-flash").generate_content(prompt)
+
+    return response.text
