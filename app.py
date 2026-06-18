a18fdddffce61a3c83a63b569f280c887d1302ab
Rashi bajpai <168172417+rashibajpai12@users.noreply.github.com>
2026-06-19 01:30:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from agents.sql_agent import generate_sql
-import streamlit as st
+from agents.failure_agent import check_sql
 from agents.insight_agent import generate_insight
 from agents.verification_agent import verify_evidence
+
+import streamlit as st
 import pandas as pd
 import plotly.express as px
 import duckdb
