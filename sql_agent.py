4d32ec10d100e0fb453d8651bb2b32938c0f3a98
Rashi bajpai <168172417+rashibajpai12@users.noreply.github.com>
2026-06-19 00:19:57 +0530

    Update sql_agent.py

--- sql_agent.py
+++ sql_agent.py
@@ -1,15 +1,3 @@
-import google.generativeai as genai
+def generate_sql(question):
 
-def generate_sql(question, columns):
-
-    prompt = f"""
-    Generate safe DuckDB SQL.
-
-    Columns:
-    {columns}
-
-    Question:
-    {question}
-    """
-
-    return model.generate_content(prompt).text
+    return "SELECT * FROM sales"
