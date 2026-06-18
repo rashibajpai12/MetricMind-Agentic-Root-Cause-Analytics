40b4792ca2847049de934f632f4851a377dbf941
Rashi bajpai <168172417+rashibajpai12@users.noreply.github.com>
2026-06-19 00:55:04 +0530

    Update sql_agent.py

--- agents/sql_agent.py
+++ agents/sql_agent.py
@@ -1,3 +1,25 @@
-def generate_sql(question):
+import google.generativeai as genai
 
-    return "SELECT * FROM sales"
+def generate_sql(question, columns):
+
+    prompt = f"""
+You are a SQL agent.
+
+Table name: sales
+
+Columns:
+{columns}
+
+User Question:
+{question}
+
+Generate ONE DuckDB SQL query only.
+
+Return SQL only.
+"""
+
+    response = genai.GenerativeModel(
+        "gemini-2.5-flash"
+    ).generate_content(prompt)
+
+    return response.text.strip()
